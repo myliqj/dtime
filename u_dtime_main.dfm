b2b8c16dec143487bb914c934c4c7bc612b942f5
myliqj <52018365@qq.com>
2019-07-26 15:17:35 +0800

    增加定时与休息，显示每次消息。

--- u_dtime_main.dfm
+++ u_dtime_main.dfm
@@ -1,8 +1,8 @@
 object frmDTime: TfrmDTime
-  Left = 468
-  Top = 298
-  Width = 389
-  Height = 187
+  Left = 203
+  Top = 202
+  Width = 887
+  Height = 327
   Caption = #23450#26102
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -15,65 +15,119 @@ object frmDTime: TfrmDTime
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
-  object lblShow: TLabel
-    Left = 16
-    Top = 8
-    Width = 3
-    Height = 13
-  end
-  object Label1: TLabel
-    Left = 90
-    Top = 95
-    Width = 12
-    Height = 13
-    Caption = #20998
-  end
-  object Label2: TLabel
-    Left = 10
-    Top = 95
-    Width = 24
-    Height = 13
-    Caption = #23450#26102
-  end
-  object btnStart: TButton
-    Left = 168
-    Top = 88
-    Width = 75
-    Height = 41
-    Hint = #24320#22987#25353' '#23450#26102' '#24320#22987#35745#26102#65292#26102#38388#21040#21518#20250' '#25552#31034#21040#26368#21069#38754
-    Caption = #24320#22987'(&S)'
+  object mOutput: TMemo
+    Left = 185
+    Top = 0
+    Width = 686
+    Height = 289
+    Align = alClient
+    Lines.Strings = (
+      '')
+    ScrollBars = ssBoth
     TabOrder = 0
-    OnClick = btnStartClick
   end
-  object btnStop: TButton
-    Left = 256
-    Top = 88
-    Width = 75
-    Height = 41
-    Hint = #32467#26463#26412#36718#35745#26102
-    Caption = #32467#26463'(&T)'
+  object Panel1: TPanel
+    Left = 0
+    Top = 0
+    Width = 185
+    Height = 289
+    Align = alLeft
+    BevelOuter = bvNone
     TabOrder = 1
-    OnClick = btnStopClick
-  end
-  object edtSJ: TEdit
-    Left = 42
-    Top = 93
-    Width = 41
-    Height = 21
-    Hint = #21462#20540#33539#22260' 0.05~10000'
-    TabOrder = 2
-    Text = '30'
-  end
-  object chkAutoStart: TCheckBox
-    Left = 10
-    Top = 120
-    Width = 135
-    Height = 17
-    Hint = #24403#19968#27425#35745#26102#32467#26463#26102#65292#26159#21542#33258#21160#24320#22987#19979#19968#27425#35745#26102
-    Caption = #33258#21160#24320#22987#19979#27425#35745#26102
-    Checked = True
-    State = cbChecked
-    TabOrder = 3
+    object lblShow: TLabel
+      Left = 8
+      Top = 8
+      Width = 51
+      Height = 25
+    end
+    object Label1: TLabel
+      Left = 109
+      Top = 109
+      Width = 12
+      Height = 13
+      Caption = #20998
+    end
+    object Label2: TLabel
+      Left = 9
+      Top = 109
+      Width = 35
+      Height = 13
+      Caption = #23450#26102
+    end
+    object Label3: TLabel
+      Left = 9
+      Top = 133
+      Width = 40
+      Height = 13
+      Caption = #20241#24687
+    end
+    object Label4: TLabel
+      Left = 109
+      Top = 133
+      Width = 12
+      Height = 13
+      Caption = #20998
+    end
+    object btnStart: TButton
+      Left = 14
+      Top = 222
+      Width = 75
+      Height = 41
+      Hint = #24320#22987#25353' '#23450#26102' '#24320#22987#35745#26102#65292#26102#38388#21040#21518#20250' '#25552#31034#21040#26368#21069#38754
+      Caption = #24320#22987'(&S)'
+      TabOrder = 0
+      OnClick = btnStartClick
+    end
+    object btnStop: TButton
+      Left = 102
+      Top = 222
+      Width = 75
+      Height = 41
+      Hint = #32467#26463#26412#36718#35745#26102
+      Caption = #32467#26463'(&T)'
+      TabOrder = 1
+      OnClick = btnStopClick
+    end
+    object edtSJ: TEdit
+      Left = 51
+      Top = 107
+      Width = 41
+      Height = 21
+      Hint = #21462#20540#33539#22260' 0.05~10000'
+      TabOrder = 2
+      Text = '30'
+    end
+    object chkAutoStart: TCheckBox
+      Left = 10
+      Top = 156
+      Width = 151
+      Height = 17
+      Hint = #24403#19968#27425#35745#26102#32467#26463#26102#65292#26159#21542#33258#21160#24320#22987#19979#19968#27425#35745#26102
+      Caption = #33258#21160#24320#22987#35745#31639' '#20241#24687
+      Checked = True
+      State = cbChecked
+      TabOrder = 3
+    end
+    object edtStopTime: TEdit
+      Left = 51
+      Top = 131
+      Width = 41
+      Height = 21
+      Hint = #21462#20540#33539#22260' 0.05~10000'
+      TabOrder = 4
+      Text = '5'
+    end
+    object chkAutoStartDS: TCheckBox
+      Left = 10
+      Top = 178
+      Width = 151
+      Height = 17
+      Hint = #24403#19968#27425#35745#26102#32467#26463#26102#65292#26159#21542#33258#21160#24320#22987#19979#19968#27425#35745#26102
+      Caption = #33258#21160#24320#22987#35745#31639' '#19979#27425' '#23450#26102
+      Checked = True
+      State = cbChecked
+      TabOrder = 5
+    end
   end
   object Timer1: TTimer
     Enabled = False
--- u_dtime_main.pas
+++ u_dtime_main.pas
@@ -16,6 +16,12 @@ type
     Label1: TLabel;
     Label2: TLabel;
     chkAutoStart: TCheckBox;
+    mOutput: TMemo;
+    Label3: TLabel;
+    edtStopTime: TEdit;
+    Label4: TLabel;
+    chkAutoStartDS: TCheckBox;
+    Panel1: TPanel;
     procedure Timer1Timer(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure btnStartClick(Sender: TObject);
@@ -25,11 +31,14 @@ type
     f_all_sj_s :integer;
     f_p_date:TDateTime;
     f_cnt :Integer;
+    f_cur_ds :boolean; // 当前是定时还是休息
   public
     { Public declarations }
     procedure showInfo(const I_Msg:string);
     procedure showTo();
     function getTimeFmt(i_sj :integer):string;
+    procedure output(i_msg :string);
+    function getdsStr():string;
   end;
 
 var
@@ -43,20 +52,27 @@ procedure TfrmDTime.showTo;
 var OldPt,NewPt:TPoint;
 begin
   //判断Application是否最小化，而不是主窗口的Handle, 使用Restore来还原
-  if (IsIconic(Application.Handle)) then
-      Application.Restore;
-  SetWindowPos(Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE);
-  SetWindowPos(Handle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE);
-  //保存鼠标位置，模拟点击窗口，然后再还原鼠标位置
-  GetCursorPos(OldPt);
-  NewPt := Point(0, 0);
-  Windows.ClientToScreen(Handle, NewPt);
-  SetCursorPos(NewPt.X, NewPt.Y);
-  mouse_event(MOUSEEVENTF_LEFTDOWN, NewPt.X, NewPt.Y, 0, 0);
-  mouse_event(MOUSEEVENTF_LEFTUP, NewPt.X, NewPt.Y, 0, 0);
-  SetCursorPos(OldPt.X, OldPt.Y);
-  
-  Position := poScreenCenter;
+  if f_cur_ds then begin
+    if (IsIconic(Application.Handle)) then  begin
+        Application.Restore;
+        Application.MainForm.WindowState := wsMaximized;
+    end;
+
+    SetWindowPos(Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE);
+    SetWindowPos(Handle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE);
+    //保存鼠标位置，模拟点击窗口，然后再还原鼠标位置
+    GetCursorPos(OldPt);
+    NewPt := Point(0, 0);
+    Windows.ClientToScreen(Handle, NewPt);
+    SetCursorPos(NewPt.X, NewPt.Y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, NewPt.X, NewPt.Y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, NewPt.X, NewPt.Y, 0, 0);
+    SetCursorPos(OldPt.X, OldPt.Y);
+
+    Position := poScreenCenter;
+  end else begin
+    Application.MainForm.WindowState := wsMinimized;
+  end;
 end;
 
 procedure TfrmDTime.Timer1Timer(Sender: TObject);
@@ -66,9 +82,20 @@ begin
     btnStopClick(Sender);
     showInfo('');
     showTo();
-    ShowMessage('指定的定时时间已到，请注意休息！');
-    if chkAutoStart.Checked then
+    output(Format('完成 %s 开始时间:%s  结束时间:%s  共 %d 秒 %s'
+      ,[ getdsStr(),FormatDateTime('yyyy-mm-dd hh:mm:ss',f_p_date)
+      ,  FormatDateTime('yyyy-mm-dd hh:mm:ss',Now()), f_cnt, getTimeFmt(f_cnt) ]));
+    //ShowMessage('指定的定时时间已到，请注意休息！');
+    if f_cur_ds then
+      output('指定的 定时 时间已到，请注意休息！')
+    else
+      output('指定的 休息 时间已到，请开始工作！');
+
+    if (f_cur_ds and chkAutoStart.Checked)
+      or (not f_cur_ds and chkAutoStartDS.Checked) then begin
+      f_cur_ds := not f_cur_ds;
       btnStartClick(Sender);
+    end;
   end;
 
   showInfo('');
@@ -78,12 +105,13 @@ procedure TfrmDTime.FormCreate(Sender: TObject);
 begin
   btnStopClick(Sender);
   Position := poScreenCenter;
+  f_cur_ds := true; // 最开始时定时
 end;
 
 procedure TfrmDTime.showInfo(const I_Msg:string);
 begin
-  lblShow.Caption := Format('开始时间:%s'#13#10'当前时间:%s'#13#10'已过 %d 秒  剩余 %d 秒'#13#10'已过 %s 剩余 %s'+ I_Msg
-    ,[ FormatDateTime('yyyy-mm-dd hh:mm:ss',f_p_date)
+  lblShow.Caption := Format('%s'#13#10'开始时间:%s'#13#10'当前时间:%s'#13#10'已过 %d 秒  剩余 %d 秒'#13#10'已过 %s 剩余 %s'+ I_Msg
+    ,[getdsStr(), FormatDateTime('yyyy-mm-dd hh:mm:ss',f_p_date)
     ,  FormatDateTime('yyyy-mm-dd hh:mm:ss',Now()), f_cnt,f_all_sj_s-f_cnt, getTimeFmt(f_cnt), getTimeFmt(f_all_sj_s-f_cnt)]) ;
 end;
 
@@ -107,11 +135,19 @@ var
 begin
   btnStop.Click;
 
-  s := Trim(edtSJ.Text);
+  if f_cur_ds then
+    s := Trim(edtSJ.Text)
+  else
+    s := Trim(edtStopTime.Text);
+
   sj := StrToFloatDef(s,30);
   if sj<0.05 then sj :=0.05;
   if sj>10000 then sj := 10000;
-  edtSJ.Text := FloatToStr(sj);
+
+  if f_cur_ds then
+    edtSJ.Text := FloatToStr(sj)
+  else
+    edtStopTime.Text := FloatToStr(sj);
 
   f_p_date := Now();
   f_cnt := 0;
@@ -119,6 +155,7 @@ begin
   btnStop.Enabled := true;
   btnStart.Enabled := False;
   Timer1.Enabled := true;
+  output('开始'+getdsStr());
 end;
 
 procedure TfrmDTime.btnStopClick(Sender: TObject);
@@ -127,6 +164,22 @@ begin
   btnStop.Enabled := false;
   btnStart.Enabled := true;
   Timer1.Enabled := false;
+  output('停止'+getdsStr());
+end;
+
+procedure TfrmDTime.output(i_msg: string);
+begin
+  mOutput.Lines.Insert(0,FormatDateTime('yyyy-mm-dd hh:mm:ss',now())
+    + ' ' + i_msg);
+  SendMessage(mOutput.Handle,WM_VSCROLL,SB_TOP,0); // 滚动到最上行
+end;
+
+function TfrmDTime.getdsStr: string;
+begin
+  if f_cur_ds then
+    Result := '定时'
+  else
+    Result := '休息';
 end;
 
 end.
